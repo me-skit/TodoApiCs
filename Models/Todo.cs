4a0f3cc043ea0c270ce9da262c17440b27821239
Meme <meme_es@hotmail.com>
2021-02-06 16:10:33 -0600

    Add base files to our .Net API project
    This commit adds base files and folders after running the instruction
    "dotnet add -new webapi TodoApiCs".

---
+++ Models/Todo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoApiCs.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string Description { get; set; }
+        [Required]
+        public int priority { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateFinish { get; set; }
+        public bool Completed { get; set; }
+    }
+}
